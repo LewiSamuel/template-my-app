7c1b0fa20e1b1c7914a8658b37a62a620ec2539c
Hugo Dias <hugo@nimbuslab.com.br>
2021-06-07 19:04:11 -0300

    Ajustes página home e estruturas.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
 
 import { User } from '../models/User';
 import { useEffect, useState } from 'react';
-import Nav from '../components/global/nav';
+
+import { Nav } from '../components/Nav';
 
 export default function Home() {
-
-  const [ Pessoa, setPessoa ] = useState<User>({ Nome: '', Email: '', Type: 0 });
+  const [Pessoa, setPessoa] = useState<User>({ Nome: '', Email: '', Type: 0 });
 
   useEffect(() => {
-    setPessoa({ Nome: 'Lewi', Email: "teste", Type: 1})
-  },[]);
+    setPessoa({ Nome: 'Lewi', Email: 'teste', Type: 1 });
+  }, []);
 
-  return (<>
-    <Head>
-      <title>Lewix</title>
-    </Head>
-    <Nav />
-    <div className={styles.container}>
-      <h1 className={styles.promisse}>Make a promise here</h1>
-    </div>
-  </>)
+  return (
+    <>
+      <Head>
+        <title>Lewix</title>
+      </Head>
+      <Nav />
+
+      <div className={styles.container}>
+        <h1 className={styles.promisse}>Make a promise here</h1>
+      </div>
+    </>
+  );
 }
